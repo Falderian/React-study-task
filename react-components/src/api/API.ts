8275cc5d14fa1ac535c06f31e4f3e9326ebc1472
Falderian <nikitastaselovich@gmail.com>
2022-10-12 17:56:28 +0300

    feat: add search logics

--- react-components/src/api/API.ts
+++ react-components/src/api/API.ts
@@ -3,11 +3,11 @@ import axios from 'axios';
 const apiKey = `0e655211503a99e2b6a8909e76f606a6`;
 const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 const apiImg = `https://image.tmdb.org/t/p/w500/pHkKbIRoCe7zIFvqan9LFSaQAde.jpg`;
-const apiSearch = `https://api.themoviedb.org/3/search/company?api_key=${apiKey}query`;
+const apiSearch = `https://api.themoviedb.org/3/search/company?api_key=${apiKey}&query`;
 
-export const baseApi = async () => {
+export const baseApi = async (url: string) => {
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(url);
     const data = await response.json();
     return data;
   } catch (error) {
--- react-components/src/components/search/search.tsx
+++ react-components/src/components/search/search.tsx
@@ -1,18 +1,39 @@
 import { baseApi } from 'api/API';
 import { ISearchCards } from 'interfaces/searchCard';
-import React from 'react';
+import React, { RefObject } from 'react';
 import { Component } from 'react';
 
 export class PageSearch extends Component {
   apiKey = `0e655211503a99e2b6a8909e76f606a6`;
   apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}`;
   apiImg = `https://image.tmdb.org/t/p/w500`;
-  apiSearch = `https://api.themoviedb.org/3/search/company?api_key=${this.apiKey}query`;
-  state = { data: [], isLoaded: false };
+  apiSearch = `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query`;
+  state = { data: [], isLoaded: false, description: false };
+
+  searchInput: RefObject<HTMLInputElement>;
+
+  constructor(props: string) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.searchInput = React.createRef();
+  }
+
+  async handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault();
+    const query = this.searchInput.current!.value;
+    this.setState({ isLoaded: false });
+    try {
+      const url = this.apiSearch + `=${query}`;
+      const data = await baseApi(url);
+      this.setState({ data: data.results, isLoaded: true });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async componentDidMount() {
     try {
-      const data = await baseApi();
+      const data = await baseApi(this.apiUrl);
       this.setState({ data: data.results, isLoaded: true });
     } catch (error) {
       console.log(error);
@@ -25,13 +46,25 @@ export class PageSearch extends Component {
     return (
       <div className="search__wrapper">
         <div className="search">
-          <form className="search-form">
+          <form className="search-form" onSubmit={(e) => this.handleSubmit(e)}>
             <label htmlFor="search-form-input" className="search-form__label">
-              Введите данные для поиска:
-              <input className="search-form__input" type="text" id="search-form-input"></input>
+              Enter data request:
+              <input
+                className="search-form__input"
+                type="text"
+                id="search-form-input"
+                ref={this.searchInput}
+              ></input>
+            </label>
+            <label className="search-form__desc-checkbox">
+              Description
+              <input
+                type="checkbox"
+                onClick={() => this.setState({ description: !this.state.description })}
+              ></input>
             </label>
             <button className="search-form_submit-btn" type="submit" id="submit-btn">
-              Отправить
+              Send
             </button>
           </form>
         </div>
@@ -51,7 +84,7 @@ export class PageSearch extends Component {
                   <div className="card__release-date">Release date: {el.release_date}</div>
                   <div className="card_popularity">Popularity: {el.popularity}</div>
                   <div className="card__votes">
-                    <div className="card__vote-count">{el.vote_count}</div>
+                    <div className="card__vote-count">Vote count: {el.vote_count}</div>
                     <div className="card__vote-average">{el.vote_average}</div>
                   </div>
                 </div>
