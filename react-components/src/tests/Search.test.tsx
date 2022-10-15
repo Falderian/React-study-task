66946d9a2c53ab153efc9da63965f23d21c2c18c
Falderian <nikitastaselovich@gmail.com>
2022-10-15 12:53:56 +0300

    upd: update api test

--- react-components/src/tests/Search.test.tsx
+++ react-components/src/tests/Search.test.tsx
@@ -6,16 +6,18 @@ import { server } from './serverMocks.ts/server';
 import { jackReacher } from './serverMocks.ts/serverRespond';
 import { act } from 'react-dom/test-utils';
 
-// describe('Search Page tests', () => {
-//   beforeAll(() => server.listen());
-//   afterEach(() => server.resetHandlers());
-//   afterAll(() => server.close());
+describe('Search Page tests', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
 
-//   it('Render page with api mock', async () => {
-//     const mockApi = jest.spyOn(PageSearch.prototype, 'baseApi').mockReturnValue(jackReacher);
-//     await act(async () => render(<PageSearch />));
-//     expect(mockApi).toHaveBeenCalledTimes(1);
-//     const cardsButtons = screen.getAllByTestId('card');
-//     expect(cardsButtons.length).toEqual(3);
-//   });
-// });
+  it('Render page with api mock', async () => {
+    const mockApi = jest.spyOn(PageSearch.prototype, 'baseApi').mockReturnValue(jackReacher);
+    await act(async () => {
+      const { container } = render(<PageSearch />);
+    });
+    expect(mockApi).toHaveBeenCalledTimes(1);
+    const cardsButtons = screen.getAllByTestId('card');
+    expect(cardsButtons.length).toEqual(3);
+  });
+});
