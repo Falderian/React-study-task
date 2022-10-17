b41c4e074ecd1fcd35513aebc24894dddd7b7ab9
Falderian <nikitastaselovich@gmail.com>
2022-10-17 21:40:26 +0300

    feat: reworked tests on search page with hooks

--- react-components/src/tests/Search.test.tsx
+++ react-components/src/tests/Search.test.tsx
@@ -1,10 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { PageSearch } from 'components/search/search';
+import { PageSearch, PageSearchOnHooks } from 'components/search/search';
 import '@testing-library/jest-dom';
-import React, { Component } from 'react';
+import React from 'react';
 import { server } from './serverMocks.ts/server';
 import { jackReacher } from './serverMocks.ts/serverRespond';
 import { act } from 'react-dom/test-utils';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Search Page tests', () => {
   beforeAll(() => server.listen());
@@ -21,5 +25,18 @@ describe('Search Page tests', () => {
     expect(mockApi).toHaveBeenCalledTimes(1);
     const cardsButtons = screen.getAllByTestId('card');
     expect(cardsButtons.length).toEqual(3);
+  });
+});
+
+describe('Search Page on Hooks tests', () => {
+  it('Render page with api mock', async () => {
+    mockedAxios.get.mockRejectedValue('Network error: Something went wrong');
+    mockedAxios.get.mockResolvedValue({ data: jackReacher });
+    await act(async () => {
+      render(<PageSearchOnHooks />);
+    });
+    const cardsButtons = screen.getAllByTestId('card');
+    expect(cardsButtons.length).toEqual(3);
+    screen.debug();
   });
 });
