058a7a53ef089fcade4e6c19b9fc9bdf500d82b3
Falderian <nikitastaselovich@gmail.com>
2022-10-31 21:26:39 +0300

    fix: fixed tests errors

--- react-components/src/tests/Search.test.tsx
+++ react-components/src/tests/Search.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { PageSearch, PageSearchOnHooks } from 'components/search/search';
+import { PageSearchOnHooks } from 'components/search/search';
 import '@testing-library/jest-dom';
 import React from 'react';
 import { server } from './serverMocks.ts/server';
@@ -9,24 +9,6 @@ import axios from 'axios';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-describe('Search Page tests', () => {
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-
-  it('Render page with api mock', async () => {
-    const mockApi = jest
-      .spyOn(PageSearch.prototype, 'baseApi')
-      .mockImplementation(() => Promise.resolve(jackReacher));
-    await act(async () => {
-      render(<PageSearch />);
-    });
-    expect(mockApi).toHaveBeenCalledTimes(1);
-    const cardsButtons = screen.getAllByTestId('card');
-    expect(cardsButtons.length).toEqual(3);
-  });
-});
 
 describe('Search Page on Hooks tests', () => {
   it('Render page with api mock', async () => {
